a3a04eeeee38f02b7eb3e49972a2bdb9b266a06b
PCPedroso <pclpedroso@gmail.com>
2024-06-12 13:09:02 -0300

    Concluido exemplo

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	// O import deve levar em consideração o que está definido no go.mod adicionando o nome do pacote a ser utilizado
+	"github.com/PCPedroso/pos-fc-intro-go-mod/math"
+)
+
+func main() {
+	m := math.Math{A: 1, B: 2}
+	fmt.Println(m.Soma())
 }
